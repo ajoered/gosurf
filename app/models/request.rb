779697b511c8110d941b32dee65858f7980f77e6
Alvarojoered <arojaspen@gmail.com>
2016-12-08 19:38:04 +0100

    Max cap for user spaces works but error does not apper

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,13 +3,17 @@ class Request < ApplicationRecord
   belongs_to :user
 
   def approve
-    self.status = true
-    self.save
+    if reached_max_users
+      errors[:base] << "You have reached the maximum number of users"
+    else
+      self.status = true
+      self.save
+    end
   end
 
   def reached_max_users
-    @true_requests = trip.last.requests.where(status: true)
-    (@true_requests.length > self.trip.max_users)? true : false
+    @true_requests = self.trip.requests.where(status: true)
+    (@true_requests.length >= self.trip.max_users)? true : false
   end
   #
   # def approve!
