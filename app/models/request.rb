787c18e172ee19ec2412280415d3c7fe8b101310
Alvarojoered <arojaspen@gmail.com>
2016-12-08 19:19:59 +0100

    Problem with Bootstrap min file comes back

--- app/models/request.rb
+++ app/models/request.rb
@@ -6,9 +6,14 @@ class Request < ApplicationRecord
     self.status = true
     self.save
   end
+
+  def reached_max_users
+    @true_requests = trip.last.requests.where(status: true)
+    (@true_requests.length > self.trip.max_users)? true : false
+  end
   #
   # def approve!
   #   update(status: true)
   # end
-   
+
 end
