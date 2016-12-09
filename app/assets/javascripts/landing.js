74be32eea20221aa14c73c9ad565cdb04e02b20f
Alvarojoered <arojaspen@gmail.com>
2016-12-09 16:34:15 +0100

    Landing.js - Loop over array of trips and get an array of coordinates

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -19,12 +19,12 @@ function fetchTrips(event) {
 }
 
 function getLatLng(response){
-	console.log(response);
   var tripsArray = response;//assign response to variable
   var geocoder   = new google.maps.Geocoder();
-	var myLatLngOriginDestination = [];
+	var countryCoordinatesArray = [];
 
 	tripsArray.forEach(function(trip) {//loop over trip array
+		console.log(trip);
 		var tripCoordinates = { //create empty object to be pushed into empty array
 			origin: null,
 			destination: null
@@ -49,84 +49,60 @@ function getLatLng(response){
 				} else {
 					alert("Something got wrong " + status);
 				}
-				myLatLngOriginDestination.push(tripCoordinates) //push result into array
 		});
-		console.log(myLatLngOriginDestination);
+		countryCoordinatesArray.push(tripCoordinates) //push result into array
+		console.log("geocoder", countryCoordinatesArray)
 	});
-
+	initMap(countryCoordinatesArray)
 };
 
-// function getCoordinates(location) {
-// 	geocoder.geocode( {
-// 		'address': location},
-// 		function(results, status) {
-// 			var myLatLngOrigin;
-// 			if (status == google.maps.GeocoderStatus.OK) {
-// 				myLatLngOrigin = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()}
-// 			} else {
-// 				alert("Something got wrong " + status);
-// 			}
-// 			return myLatLngOrigin;
-// 	});
-// }
-
-function initMap(positions) {
-	console.log(positions)
-	var myLatLngOrigin = positions[0];
-	var myLatLngDestination = positions[1];
-	console.log(myLatLngOrigin)
-	console.log(myLatLngDestination)
-
+function initMap(coordinateArray) {
+	console.log("initmap", coordinateArray);
+	var tripCoordinateArray = coordinateArray
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
-    center: myLatLngOrigin
+    center: tripCoordinateArray[0].origin
   });
 
-  var flightPlanCoordinates = [
-  ];
+	tripCoordinateArray.forEach(function(trip) {
+		var flightPlanCoordinates = [];
 
-	flightPlanCoordinates.unshift(myLatLngOrigin);
-	flightPlanCoordinates.push(myLatLngDestination);
+		flightPlanCoordinates.unshift(trip.destination);
+		flightPlanCoordinates.push(trip.origin);
 
 
-  var flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
-  });
+	  var flightPath = new google.maps.Polyline({
+	    path: flightPlanCoordinates,
+	    geodesic: true,
+	    strokeColor: '#FF0000',
+	    strokeOpacity: 1.0,
+	    strokeWeight: 2
+	  });
 
-	var infowindow = new google.maps.InfoWindow({
-		content: contentString
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString
+		});
+		var contentString = '<div id="content">'+
+			'<div id="siteNotice">'+
+			'</div>'+
+			'<h1 id="firstHeading" class="firstHeading">Surf Trip</h1>'+
+			'<div id="bodyContent">'+
+			'<p><b>Generic Surftrip</b>'
+			'</div>'+
+			'</div>';
+
+		var marker = new google.maps.Marker({
+	    position: trip.destination,
+	    map: map,
+			title: 'Surftrip'
+	  });
+		marker.addListener('click', function() {
+		infowindow.open(map, marker);
+		});
+
+	  flightPath.setMap(map);
 	});
-	var contentString = '<div id="content">'+
-		'<div id="siteNotice">'+
-		'</div>'+
-		'<h1 id="firstHeading" class="firstHeading">Surf Trip</h1>'+
-		'<div id="bodyContent">'+
-		'<p><b>Generic Surftrip</b>'
-		'</div>'+
-		'</div>';
-
-	var marker = new google.maps.Marker({
-    position: myLatLngDestination,
-    map: map,
-		title: 'Surftrip'
-  });
-	marker.addListener('click', function() {
-	infowindow.open(map, marker);
-	});
-
-  flightPath.setMap(map);
 }
-
-function updateMap(response) {
-  var tripArray = response
-  tripArray.forEach(function (trip){
-		console.log(trip)
-  });
-};
 
 function handleError (error) {
     console.log('Error!');
